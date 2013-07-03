98b0a0acfa27609cc5c44ef9012bb8890caf7b15
kperusko <private@mail.fake>
2013-07-03 22:09:22 +0200

    Fixed error in linking theme file

--- link_files.zsh
+++ link_files.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
-ln -sf $(pwd)/.oh-my-zsh/themes/kperusko.zsh-theme
+ln -sf $(pwd)/.oh-my-zsh/themes/kperusko.zsh-theme $HOME/.oh-my-zsh/themes/kperusko.zsh-theme
