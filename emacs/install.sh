b2317646bde64fff6f8491a13548145a39b779d2
kperusko <private@mail.fake>
2013-07-09 10:30:37 +0200

    Fixed install.sh for emacs to pull ubuntu_settings repo

--- emacs/install.sh
+++ emacs/install.sh
@@ -4,10 +4,14 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
-# git pull and install dotfiles as well
+# git pull and install configs as well
 cd $HOME
+if [ ! -d ./ubuntu_settings/ ]; then
+    git clone https://kperusko@bitbucket.org/kperusko/ubuntu_settings/
+fi
+
 if [ -d .emacs.d/ ]; then
-mv .emacs.d .emacs.d-old
+    mv .emacs.d .emacs.d-old
 fi
 
 ln -sf ubuntu_settings/emacs/.emacs.d .
