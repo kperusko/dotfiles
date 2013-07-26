28e29f04aaf05c961b9ea9fc76be84f3c942dd4b
kperusko <private@mail.fake>
2013-07-26 10:12:21 +0200

    Added anything and php-completion for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -91,6 +91,25 @@
 '(setq mumamo-per-buffer-local-vars
 (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
 
+;; ----------------------
+;; -- php-autocomplete --
+;; ----------------------
+(add-hook 'php-mode-hook
+          (lambda ()
+            (require 'php-completion)
+            (php-completion-mode t)
+            (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)))
+
+(add-hook  'php-mode-hook
+           (lambda ()
+             (when (require 'auto-complete nil t)
+               (make-variable-buffer-local 'ac-sources)
+               (add-to-list 'ac-sources 'ac-source-php-completion)
+;     if you like patial match,
+;;    use `ac-source-php-completion-patial' instead of `ac-source-php-completion'.
+               (add-to-list 'ac-sources 'ac-source-php-completion-patial)
+               (auto-complete-mode t))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
