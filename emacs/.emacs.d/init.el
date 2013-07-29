9e2e087fd4298530223c9b6e08b341e40e170962
kperusko <private@mail.fake>
2013-07-29 20:17:35 +0200

    Fixed tab positions in emacs configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -28,18 +28,6 @@
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
 (setq inhibit-startup-screen t)
-(setq default-tab-width 4)
-(setq tab-width 4)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(autopair-global-mode t)
- '(custom-enabled-themes (quote (wombat)))
- '(global-linum-mode t)
- '(make-backup-files nil))
 
 ;; ------------
 ;; -- Macros --
@@ -123,7 +111,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mumamo-background-chunk-major ((t nil)))
  '(mumamo-background-chunk-submode1 ((t nil)))
  '(mumamo-background-chunk-submode2 ((t nil)))
  '(mumamo-background-chunk-submode3 ((t nil)))
  '(mumamo-background-chunk-submode4 ((t nil))))
+
+(setq default-tab-width 4)
+(setq tab-width 4)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(autopair-global-mode t)
+ '(custom-enabled-themes (quote (wombat)))
+ '(global-linum-mode t)
+ '(make-backup-files nil)
+ '(nxml-child-indent 4)
+ '(tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60)))
