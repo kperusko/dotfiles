dcea24542aa1627844079cbb071985d1681f0089
kperusko <private@mail.fake>
2013-09-05 11:14:04 +0200

    Added gnome terminal config into zsh config

--- zsh/install.sh
+++ zsh/install.sh
@@ -11,8 +11,8 @@ sudo apt-get install -y git-core
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 #The .zshrc is configured to use a plugin for the syntax highlighting
-mkdir ~/.oh-my-zsh/custom/plugins
-cd ~/.oh-my-zsh/custom/plugins
+mkdir $HOME/.oh-my-zsh/custom/plugins
+cd $HOME/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
 #Clone this repo
@@ -26,3 +26,18 @@ fi
 cd $HOME/dotfiles/zsh
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
 ln -sf $(pwd)/.oh-my-zsh/themes/kperusko.zsh-theme $HOME/.oh-my-zsh/themes/kperusko.zsh-theme
+
+#Copy terminal preferences for gnome terminal
+#These are not dependent on zsh
+cd $HOME
+if [ ! -d "./.gconf/apps/gnome-terminal/" ]; then
+	echo "Directory .gconf/apps/gnome-terminal/ doesn't exists. Gnome terminal preferences not installed!"
+	exit 0;
+fi
+
+if [ -f "./.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml" ]; then
+	mv "./.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml" "./.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml.bak"
+fi
+
+cd $HOME/dotfiles/zsh
+ln -sf $(pwd)/"%gconf.xml" "$HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml"
