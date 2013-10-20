80f354a4ada65602f9b37731b0e6df2c0744e869
Kristijan Perusko <kristijan.perusko@yahoo.com>
2013-10-20 11:18:27 +0200

    Added initial script for fixing the two finger scroll settings in ubuntu

---
+++ ubuntu-settings/synaptics-two-finger-scroll.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+synclient VertTwoFingerScroll=1
+synclient HorizTwoFingerScroll=1
+synclient EmulateTwoFingerMinZ=48
+synclient EmulateTwoFingerMinW=5
+synclient VertScrollDelta=-200
+synclient HorizScrollDelta=-200
