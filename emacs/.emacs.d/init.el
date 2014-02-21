c648094d05992c0484c02c6ccd5994ad5ef1d87f
kperusko <private@mail.fake>
2014-02-21 12:33:31 +0100

    Changed autocomplete show timout for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -188,6 +188,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-auto-show-menu 0.1)
  '(autopair-global-mode t)
  '(custom-enabled-themes (quote (wombat)))
  '(global-linum-mode t)
